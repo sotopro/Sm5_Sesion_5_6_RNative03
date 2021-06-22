6614352b509bd7cfd23024c7fe0d38975fc469a4
Daniel Soto <dasg.1993@gmail.com>
2021-06-21 21:43:55 -0500

    [feature} - create scroll top component with style componets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,125 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Product from './shared/components/Product';
+import Progress from './shared/components/progress/style';
+// import Product from './shared/components/Product';
+// import styled from 'styled-components';
+
+// const Title = styled.h1`
+//   font-size: 20px;
+//   text-align: center;
+//   color: palevioletred;
+// `;
+
+// const style = {
+//   h1: {
+//     fontSize: '20px',
+//     textAlign: 'center',
+//     color: 'palevioletred',
+//   }
+// };
 
 const App = () => {
+  const [scrollPosition, setScrollPosition] = useState(0)
+  const getDocHeight = () => {
+    return Math.max(
+      document.body.scrollHeight, document.documentElement.scrollHeight,
+      document.body.offsetHeight, document.documentElement.offsetHeight,
+      document.body.clientHeight, document.documentElement.clientHeight
+    )
+  }
+  let calculateScrollDistance = () => {
+    const scrollTop = window.pageYOffset;
+    const windowHeight = window.innerHeight
+    const docHeight = getDocHeight();
+
+    const totalDocScrollLenght = docHeight - windowHeight;
+    const scrollPosition = Math.floor(scrollTop / totalDocScrollLenght * 100)
+    setScrollPosition(scrollPosition)
+  }
+  const listenScrollEvent = () => {
+    requestAnimationFrame(() => {
+      calculateScrollDistance();
+    })
+  }
+
+  useEffect(() => {
+    document.addEventListener("scroll", () => listenScrollEvent());
+    return () => {
+      document.removeEventListener('scroll', () => listenScrollEvent());
+    }
+  })
+
   return (
-    <div className="container">
-      <Product />
+    <div className="app">
+      <Progress scroll={`${scrollPosition}%`} />
+      <header></header>
+      <main>
+        <h1>Lorem Ipsum</h1>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        </p>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        </p>
+        <h1>Lorem Ipsum</h1>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        </p>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        </p>
+        <h1>Lorem Ipsum</h1>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        </p>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        </p>
+        <h1>Lorem Ipsum</h1>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        </p>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        </p>
+        <h1>Lorem Ipsum</h1>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        </p>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        </p>
+        <h1>Lorem Ipsum</h1>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        </p>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        </p>
+        <h1>Lorem Ipsum</h1>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        </p>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        </p>
+        <h1>Lorem Ipsum</h1>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        </p>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        </p>
+        <h1>Lorem Ipsum</h1>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        </p>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        </p>
+      </main>
+      {/* <Title>Hello</Title>
+      <Product /> */}
     </div>
   );
 }
--- src/shared/components/Product/index.jsx
+++ src/shared/components/Product/index.jsx
@@ -51,7 +51,6 @@ const Product = () => {
                         </div>
                     </div>
                 ))}
-                
             </div>
         </div>
     )
