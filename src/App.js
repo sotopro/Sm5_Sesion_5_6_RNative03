d23153e98de1fb2b1c2ae4b889b791c1823c3c22
Daniel Soto <dasg.1993@gmail.com>
2021-06-21 20:12:13 -0500

    [feature] - create products & add state cart & logic to add & remove products carts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Product from './shared/components/Product';
 
 const App = () => {
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <Product />
     </div>
   );
 }
